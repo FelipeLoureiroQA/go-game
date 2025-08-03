82c2cab886a6f902eddecfc9cf2a97998e549683
FelipeLoureiroQA <drewaver@gmail.com>
2025-08-03 19:03:20 -0300

    feat: receitas

---
+++ receitas.go
@@ -0,0 +1,20 @@
+package main
+
+var Receitas = []Pocao{
+	{
+		Nome: "Poção de Cura",
+		Combinacao: []string{
+			"Raiz-de-Cura",
+			"Agua-Pura",
+		},
+		Descricao: "Uma poção que restaura a saúde do aventureiro.",
+	},
+	{
+		Nome: "Poção do Sono",
+		Combinacao: []string{
+			"Erva-do-Sono",
+			"Sais-da-Terra",
+		},
+		Descricao: "Uma poção que faz com que criaturas adormeçam.",
+	},
+}
