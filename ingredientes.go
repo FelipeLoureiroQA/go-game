2d753db98fe75e191fa98ab4a9736399cfb41b3c
FelipeLoureiroQA <drewaver@gmail.com>
2025-08-03 19:02:22 -0300

    feat: ingredientes

---
+++ ingredientes.go
@@ -0,0 +1,11 @@
+package main
+
+// ItensDisponiveis é a lista de todos os itens que podem existir no jogo.
+var ItensDisponiveis = []Item{
+	{Nome: "Erva-do-Sono"},
+	{Nome: "Raiz-de-Cura"},
+	{Nome: "Flor-da-Luz"},
+	{Nome: "Madeira-Antiga"},
+	{Nome: "Sais-da-Terra"},
+	{Nome: "Agua-Pura"},
+}
